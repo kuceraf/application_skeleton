12a679aec334e8102768ab21bb4d870c40efecba
Filip Kucera <kucerafilip@gmail.com>
2016-11-06 12:20:57 +0100

    logback

--- rest-server/src/main/java/cz/fku/controller/TestController.java
+++ rest-server/src/main/java/cz/fku/controller/TestController.java
@@ -1,5 +1,9 @@
 package cz.fku.controller;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,9 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/testEcho")
 public class TestController {
+    final Logger logger = LoggerFactory.getLogger(TestController.class);
     @RequestMapping(value = "/{testId}", method = RequestMethod.GET)
     public String test(@PathVariable String testId) {
+        testLogger();
 //      return value is automatically bound to the web response body
         return testId;
+    }
+//  logback info:  http://logback.qos.ch/manual/architecture.html
+    private void testLogger() {
+        logger.info("----------------FKU----------------");
+        // print internal state
+//        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//        StatusPrinter.print(lc);
     }
 }
